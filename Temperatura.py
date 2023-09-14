6a3cea13bdad380954bd59a58a0e9d0f568c9ca6
Mario silva <Sztriton23@gmail.com>
2023-09-14 11:53:12 -0300

    Temperatura y Masa

---
+++ Temperatura.py
@@ -0,0 +1,14 @@
+def celsius_fahrenheit(celsius):
+    formula = (celsius * 9/5) + 32
+    return f"El resultado es {formula}"
+
+
+def celsius_kelvin(celsius):
+    formula = 0
+    return f"El resultado es {formula}"
+
+
+if __name__ == '__main__':
+    celsius = 10
+    print(celsius_fahrenheit(celsius))
+    print(celsius_kelvin(celsius))
